5affb63d2a9e3de1440fb099f4ba64fcd66f0c7d
Drpmartins <abonyiizuchukwu@gmail.com>
2023-10-28 21:37:10 -0700

    sorting algorithms

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -75,13 +75,13 @@ void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow)
 
 /**
  * bitonic_sort - Sort an array of integers in ascending
- * order using the bitonic sort algorithm.
+ *                order using the bitonic sort algorithm.
  * @array: An array of integers.
  * @size: The size of the array.
+ *
  * Description: Prints the array after each swap. Only works for
- //* size = 2^k where k >= 0 (ie. size equal to powers of 2).
+ * size = 2^k where k >= 0 (ie. size equal to powers of 2).
  */
-
 void bitonic_sort(int *array, size_t size)
 {
 	if (array == NULL || size < 2)
